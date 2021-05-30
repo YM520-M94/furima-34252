42f32a0fb0b97de1fd45cb37cb0c5a88deb607e6
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-05-30 12:11:55 +0900

    ユーザー管理機能 単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角(漢字・ひらがな・カタカナ)を使用してください' } do
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角(カタカナ)を使用してください' } do
-    validates :first_name_reading, presence: true
-    validates :last_name_reading, presence: true
-  end
-  
-  validates :birthday, presence: true
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-    validates :password, format: {with: PASSWORD_REGEX, allow_blank: true, message: 'には半角英字と数字の両方を含めて設定してください' }
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
-
-  validates :encrypted_password, :password, :password_confirmation, length:{minimum:6}
+  validates :nickname,           presence: true
+  validates :email,              presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,           presence: true, format: {with: /\A[a-z\d]{6,}+\z/i}
+  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_reading,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_reading, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,           presence: true
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
+  password = Faker::Internet.password(min_length: 6)
+
   factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {password}
+    password_confirmation {password}
+    last_name             {'鈴木'}
+    first_name            {'一郎'}
+    last_name_reading     {'スズキ'}
+    first_name_reading    {'イチロウ'}
+    birthday              {'2000-01-01'}
     
   end
 end
