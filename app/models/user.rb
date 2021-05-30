dc3cf4df42be2f815e38f57557509c1c593e4e77
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-05-30 16:26:42 +0900

    バリデーション email欄　削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :nickname,           presence: true
   
-  validates :email,              presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, allow_blank: true
   validates :password,           presence: true, format: {with: /\A[a-z\d]{6,}+\z/i},                   allow_blank: true
   validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龠々]+\z/ },               allow_blank: true
   validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龠々]+\z/ },               allow_blank: true
