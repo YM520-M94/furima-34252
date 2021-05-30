fbee520f8025a8931b4822fad859c8c4f5521efd
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-05-30 16:25:36 +0900

    バリデーション　正規表現　記述変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   
   validates :email,              presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, allow_blank: true
   validates :password,           presence: true, format: {with: /\A[a-z\d]{6,}+\z/i},                   allow_blank: true
-  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ },               allow_blank: true
-  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ },               allow_blank: true
+  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龠々]+\z/ },               allow_blank: true
+  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龠々]+\z/ },               allow_blank: true
   validates :last_name_reading,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ },                    allow_blank: true
   validates :first_name_reading, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ },                    allow_blank: true
   validates :birthday,           presence: true
