28bfaece3feaa6bd64bad918049057e38792e381
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-03 01:31:39 +0900

    products (createアクション)　設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,13 @@ class ProductsController < ApplicationController
     @products = Product.new
   end
 
+  def create
+    Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:product, :listing_person, :price, :category_id, :product_state_id, :delivery_charge_id, :prefecture_id, :delivery_date_id)
+  end
+
 end
