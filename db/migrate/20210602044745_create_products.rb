23c766f56e3574355b344f892adaacf6e5c77fc4
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-03 00:56:39 +0900

    products テープル カラム設定

--- db/migrate/20210602044745_create_products.rb
+++ db/migrate/20210602044745_create_products.rb
@@ -2,6 +2,15 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
 
+      t.string     :product,            null: false
+      t.text       :listing_person,     null: false
+      t.integer    :price,              null: false
+      t.integer    :category_id,        null: false
+      t.integer    :product_state_id,   null: false
+      t.integer    :delivery_charge_id, null: false
+      t.integer    :prefecture_id,      null: false
+      t.integer    :delivery_date_id,   null: false
+      t.references :user,               foreign_key: true
       t.timestamps
     end
   end
