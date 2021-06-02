24608e39acc395cecf1bbab5ebbeb98c4fbcbe98
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-03 01:43:32 +0900

    products(バリデーション) + user間とのアソシエーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :product
+    validates :listing_person
+    validates :price
+    validates :category_id
+    validates :product_state_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
+
 end
