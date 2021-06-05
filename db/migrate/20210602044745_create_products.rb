443e41e63d07dc47c716b85e32e83fcddb75c1fe
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-05 17:34:13 +0900

    ImageMagick インストール

--- db/migrate/20210602044745_create_products.rb
+++ db/migrate/20210602044745_create_products.rb
@@ -10,7 +10,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.integer    :delivery_charge_id, null: false
       t.integer    :prefecture_id,      null: false
       t.integer    :delivery_date_id,   null: false
-      t.references :user,               foreign_key: true
+      t.references :user,               null: false, foreign_key: true
       t.timestamps
     end
   end
