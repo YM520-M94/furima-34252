d51d0e036df5195054995cb5f3b292e09cc62600
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-06 02:05:44 +0900

    ActiveHash)アソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :productstate
+  belongs_to :deliverycharge
+  belongs_to :prefecture
+  belongs_to :deliverydate
+
   belongs_to :user
   has_one_attached :image
 
