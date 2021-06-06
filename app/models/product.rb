e0982646e5c7620336eef12010f9abf42011160c
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-06 13:49:27 +0900

    product) バリデーション変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,4 @@
 class Product < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :productstate
-  belongs_to :deliverycharge
-  belongs_to :prefecture
-  belongs_to :deliverydate
 
   belongs_to :user
   has_one_attached :image
@@ -13,6 +7,7 @@ class Product < ApplicationRecord
     validates :product
     validates :listing_person
     validates :price
+
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :product_state_id
@@ -20,6 +15,19 @@ class Product < ApplicationRecord
       validates :prefecture_id
       validates :delivery_date_id
     end
+
+    with_options format: { with: /\A[0-9]+\z/ }, length: {minimum: 3, maxinum: 7}, 
+      numericality: { only_integer: true, greater_than: 300, less_than: 10000000 } do
+      validates :price
+    end
+
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :productstate
+  belongs_to :deliverycharge
+  belongs_to :prefecture
+  belongs_to :deliverydate
 
 end
