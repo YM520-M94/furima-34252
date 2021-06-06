5cb2a7367f0005ba729364afe1e9714059dfd971
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-07 06:40:23 +0900

    商品出品機能　単体テスト　設定

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :product do
+
+    product            {Faker::Lorem.sentence}
+    listing_person     {Faker::Lorem.sentence}
+    price              {1000}
+    category_id        {3}
+    product_state_id   {3}
+    delivery_charge_id {3}
+    prefecture_id      {3}
+    delivery_date_id   {3}
+    association :user
     
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
