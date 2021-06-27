0a3f1acf0bc00d796b4199a1275778fe18bb59aa
YM520-M94 <79740262+YM520-M94@users.noreply.github.com>
2021-06-27 09:40:47 +0900

    クレジット購入機能の実装

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -1,13 +1,19 @@
 class PurchasersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!,      only: [:index, :create]
+  before_action :set_params,              only: [:index, :create, :move_to_index, :move_to_index_purchaser]
+  before_action :move_to_index,           only: [:index, :create]
+  before_action :move_to_index_purchaser, only: [:index, :create]
 
   def index
     @purchaser_address = PurchaserAddress.new
+    #@product = Product.find(params[:product_id])
   end
 
   def create
     @purchaser_address = PurchaserAddress.new(purchaser_params)
+    #@product = Product.find(params[:product_id])
     if @purchaser_address.valid?
+      pay_product
       @purchaser_address.save
       redirect_to root_path
     else
@@ -18,7 +24,34 @@ class PurchasersController < ApplicationController
   private
 
   def purchaser_params
-    params.require(:purchaser_address).permit(:postal_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+    params.require(:purchaser_address).permit(:postal_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: purchaser_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_params
+    @product = Product.find(params[:product_id])
+  end
+
+  def move_to_index
+    #@product = Product.find(params[:product_id])
+    if current_user.id == @product.user_id
+    redirect_to root_path 
+    end
+  end
+
+  def move_to_index_purchaser
+    #@product = Product.find(params[:product_id])
+    if @product.purchaser.present?
+      redirect_to root_path 
+    end
   end
 
 end
--- app/models/purchaser_address.rb
+++ app/models/purchaser_address.rb
@@ -1,9 +1,9 @@
 class PurchaserAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number
+  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :product_id, :prefecture_id, :city, :block
+    validates :user_id, :product_id, :prefecture_id, :city, :block, :token
     validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A\d{10,11}\z/ , message: "Phone Number is too long (maximum is 11 characters)"}
   end
